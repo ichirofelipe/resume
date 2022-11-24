dda9266f41441a96879d13b51893bbcf9375da21
ichirofelipe <ichiro.felipe1696@gmail.com>
2022-11-25 00:29:33 +0800

    updated resume PIXI and TS

--- index.php
+++ index.php
@@ -7,11 +7,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ichiro Felipe</title>
     <link rel="stylesheet" href="dist/css/style.css"></link>
+    <!-- <script src="dist/js/jspdf.js"></script> -->
+    <script src="dist/js/jspdf.js"></script>
+    <script src="dist/js/jquery.js"></script>
 </head>
 
 <body>
-
-    <section class="my_cv grid grid-cols-3">
+    <section id="my_cv" class="my_cv grid grid-cols-3">
         <aside class="sidebar--dark cols-span-1 text--dwhite">
             <div class="sidebar_inner">
                 <div class="profile profile--round mb-30">
@@ -40,12 +42,12 @@
                     <div class="mb-20">
                         <strong class="title title-nm title-regular family-arial">Pedion D. Octavio</strong>
                         <p class="description mb-10">ICT Faculty</p>
-                        <p class="description"><strong>T:</strong> 09777604481</p>
+                        <p class="description"><strong>Contact #:</strong> 09777604481</p>
                     </div>
                     <div class="mb-2">
-                        <strong class="title title-nm title-regular family-arial">Christian Rubiato</strong>
-                        <p class="description mb-10">IT Instructor</p>
-                        <p class="description"><strong>T:</strong> 09971698978</p>
+                        <strong class="title title-nm title-regular family-arial">Chris Navarroza</strong>
+                        <p class="description mb-10">Senior Web Developer</p>
+                        <p class="description"><strong>Email:</strong> c.navarroza@psa.gov.ph</p>
                     </div>
                 </section>
             </div>
@@ -55,7 +57,7 @@
                 <section class="heading grid grid-cols-2 mt-30 mb-30">
                     <div>
                         <h1 class="title title-regular title-xl m-0 text--dark ls-2">Ichiro <br>Felipe</h1>
-                        <span class="title_sublabel">Full Stack Developer</span>
+                        <span class="title_sublabel">Full Stack Web Developer</span>
                     </div>
                     <div>
                         <ul class="info_list m-0 list-style-none">
@@ -70,6 +72,7 @@
                             <li>
                                 <span><!-- SVG ICON --></span>
                                 <p class="m-0 text--dark description description--sm">ichiro.felipe1696@gmail.com</p>
+                                <p class="m-0 text--dark description description--sm">https://github.com/ichirofelipe</p>
                             </li>
                         </ul>
                     </div>
@@ -80,29 +83,55 @@
                     <ul class="timeline p-0 m-0 pt-5">
                         <li class="timeline_inner grid grid-cols-3">
                             <div class="timeline_info cols-span-1 pb-20">
-                                <strong class="title title-sm family-arial text--dark">Alorica</strong>
+                                <strong class="description title title-sm family-arial text--dark mb-5">Alorica</strong>
                                 <p class="description text--ldark">Makati <br>Jan 2016 - Sep 2016</p>
                             </div>
                             <div class="timeline_additional cols-span-2 pb-20">
                                 <strong>TSR</strong>
+                                <p class="description">- Helping customer solve their issues about the services we offer. (Internet, TV, Telephone)</p>
                             </div>
                         </li>
                         <li class="timeline_inner grid grid-cols-3">
                             <div class="timeline_info cols-span-1 pb-20">
-                                <strong class="title title-sm family-arial text--dark">Firstcom <br>Solutions</strong>
+                                <strong class="description title title-sm family-arial text--dark mb-5">Firstcom <br>Solutions</strong>
                                 <p class="description text--ldark">Mar 2019 - Dec 2019</p>
                             </div>
                             <div class="timeline_additional cols-span-2 pb-20">
                                 <strong>Freelance Frontend Developer</strong>
+                                <p class="description">Translating Web Designs/Wireframes into an actual working website (Opencart, CSS Bootstrap, Jquery)</p>
                             </div>
                         </li>
                         <li class="timeline_inner grid grid-cols-3">
                             <div class="timeline_info cols-span-1">
-                                <strong class="title title-sm family-arial text--dark">Chanz IT/ Chanz <br>Global</strong>
-                                <p class="description text--ldark">Ortigas <br>Jan 2020 - Present</p>
+                                <strong class="description title title-sm family-arial text--dark mb-5">Chanz IT/ Chanz <br>Global</strong>
+                                <p class="description text--ldark">Ortigas <br>Jan 2020 - June 2022</p>
+                            </div>
+                            <div class="timeline_additional cols-span-2 pb-20">
+                                <strong>Full Stack Developer</strong>
+                                <p class="description text--ldark">Translating designs, maintaining, fixing bugs, and adding new features to existing websites (Laravel/Wordpress, Tailwind/SCSS, Vue/Jquery, Mysql, Webpack)</p>
+                                <br>
+                                <p class="description"><strong>Some of my projects here:</strong></p>
+                                <p class="description text--ldark">- Filipinoart.ph</p>
+                                <p class="description text--ldark">- latinweekender.com</p>
+                                <p class="description text--ldark">- totaalklimaat.nl</p>
+                                <p class="description text--ldark">- lucasvanleydenfonds.nl</p>
+                                <p class="description text--ldark">- abrahamart.com</p>
+                            </div>
+                        </li>
+                        <li class="timeline_inner grid grid-cols-3 mb-10">
+                            <div class="timeline_info cols-span-1">
+                                <strong class="description title title-sm family-arial text--dark mb-5">88 Group</strong>
+                                <p class="description text--ldark">Pasay <br>June 2020 - Present</p>
                             </div>
                             <div class="timeline_additional cols-span-2">
                                 <strong>Full Stack Developer</strong>
+                                <p class="description text--ldark">Creating websites from scratch starting from database planning to UI/UX Development with HTML canvas animation (Node.js, native PHP, MSSQL/MYSQL, JS/TS, Pixi.js)</p>
+                                <br>
+                                <p class="description"><strong>Some of my projects here:</strong></p>
+                                <p class="description text--ldark">- Bookmark website (in my github)</p>
+                                <p class="description text--ldark">- multivendor website (in my github)</p>
+                                <p class="description text--ldark">- OMG betting game website</p>
+                                <p class="description text--ldark">- Slot Game (Ocean Story) (in my github)</p>
                             </div>
                         </li>
                     </ul>
@@ -113,7 +142,7 @@
                     <ul class="timeline p-0 m-0 pt-5">
                         <li class="timeline_inner grid grid-cols-3">
                             <div class="timeline_info cols-span-1 pb-20">
-                                <p class="title title-sm family-arial text--dark">STI College Novaliches</p>
+                                <p class="title title-sm family-arial text--dark mb-5 mt-0">STI College Novaliches</p>
                                 <p class="description text--ldark">Quezon City <br>2019</p>
                             </div>
                             <div class="timeline_additional cols-span-2 pb-20">
@@ -127,36 +156,44 @@
                 <section class="info_block pt-30">
                     <h4 class="title title-md title-regular mt-0 mb-10 pb-5 border-b-floating-2 no-exceed">Skills</h4>
                     <div class="grid grid-cols-2 gap-x-20">
-                        <div>
+                        <div class="mb-5">
                             <p class="title title-sm family-arial text--dark mb-5">PHP</p>
-                            <div class="progress-bar progress-bar--75"></div>
-                        </div>
-                        <div>
-                            <p class="title title-sm family-arial text--dark mb-5">MYSql</p>
-                            <div class="progress-bar progress-bar--75"></div>
-                        </div>
-                        <div>
-                            <p class="title title-sm family-arial text--dark mb-5">SASS/SCSS</p>
-                            <div class="progress-bar progress-bar--75"></div>
-                        </div>
-                        <div>
-                            <p class="title title-sm family-arial text--dark mb-5">BEM</p>
-                            <div class="progress-bar progress-bar--75"></div>
-                        </div>
-                        <div>
-                            <p class="title title-sm family-arial text--dark mb-5">PDF/IMG/WIREFRAME TO ACTUAL WEB</p>
                             <div class="progress-bar progress-bar--100"></div>
                         </div>
-                        <div>
+                        <div class="mb-5">
+                            <p class="title title-sm family-arial text--dark mb-5">MYSql/MSSQL</p>
+                            <div class="progress-bar progress-bar--100"></div>
+                        </div>
+                        <div class="mb-5">
+                            <p class="title title-sm family-arial text--dark mb-5">CSS/SCSS/BOOTSTRAP/TAILWIND</p>
+                            <div class="progress-bar progress-bar--100"></div>
+                        </div>
+                        <div class="mb-5">
+                            <p class="title title-sm family-arial text--dark mb-5">DESIGN TO ACTUAL WEB</p>
+                            <div class="progress-bar progress-bar--100"></div>
+                        </div>
+                        <div class="mb-5">
                             <p class="title title-sm family-arial text--dark mb-5">LARAVEL</p>
                             <div class="progress-bar progress-bar--75"></div>
                         </div>
-                        <div>
+                        <div class="mb-5">
                             <p class="title title-sm family-arial text--dark mb-5">WORDPRESS</p>
                             <div class="progress-bar progress-bar--75"></div>
                         </div>
-                        <div>
+                        <div class="mb-5">
                             <p class="title title-sm family-arial text--dark mb-5">VUE</p>
+                            <div class="progress-bar progress-bar--75"></div>
+                        </div>
+                        <div class="mb-5">
+                            <p class="title title-sm family-arial text--dark mb-5">NODE</p>
+                            <div class="progress-bar progress-bar--75"></div>
+                        </div>
+                        <div class="mb-5">
+                            <p class="title title-sm family-arial text--dark mb-5">TS/JS/Jquery</p>
+                            <div class="progress-bar progress-bar--100"></div>
+                        </div>
+                        <div class="mb-5">
+                            <p class="title title-sm family-arial text--dark mb-5">PIXI.JS</p>
                             <div class="progress-bar progress-bar--75"></div>
                         </div>
                     </div>
@@ -168,11 +205,12 @@
                     <ul class="timeline p-0 m-0 pt-5">
                         <li class="timeline_inner grid grid-cols-3">
                             <div class="timeline_info cols-span-1 pb-20">
-                                <p class="title title-sm family-arial text--dark">FY Designs</p>
+                                <p class="description title title-sm family-arial text--dark mb-5">FY Designs</p>
                                 <p class="description text--ldark">Quezon City <br>Apr 2018 - May 2018</p>
                             </div>
                             <div class="timeline_additional cols-span-2 pb-20">
                                 <strong>Frontend Developer</strong>
+                                <p class="description text--ldark">- Assisted on fixing current website layout and responsiveness</p>
                             </div>
                         </li>
                     </ul>
@@ -180,7 +218,24 @@
             </div>
         </aside>
     </section>
+    <button id="print" onclick="generatePDF()">PRINT RESUME</button>
 
+    <script>
+        function generatePDF() {
+            const element = document.getElementById('my_cv');
+            var opt = {
+            margin:       0,
+            filename:     'ichiro_resume.pdf',
+            image:        { type: 'jpeg', quality: 0.98 },
+            html2canvas:  { scale: 1, scrollY: 0, scrollX: 0, x: 360, y: 0},
+            jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+            };
+
+            // New Promise-based usage:
+            html2pdf().set(opt).from(element).save();
+
+        }
+    </script>
 </body>
 
 </html>
